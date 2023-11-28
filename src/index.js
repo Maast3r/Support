69b42c8a4be834614b99ca85dd1c724c35e136bc
Maast3r <maastergotmail@gmail.com>
2023-11-28 13:40:44 -0800

    start adding quadrakill. more to come

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-const { app, Menu, Tray } = require('electron');
+const { app, BrowserWindow, Menu, Tray } = require('electron');
 const path = require('path');
 
-const { loadStore } = require('./store');
 const { getAllPlugins } = require('./plugins');
+const {
+    ELIGIBLE_PLAYERS,
+    GAME_EVENTS_TO_AUDIO,
+} = require('./quadrakillSettings');
+const { loadStore } = require('./store');
 
 /**
  * todo:
  * add quadrakill
+ * - save sound files to store?
+ * - create gui to manage sound files?
+ *
  * create exe
  *
  */
@@ -43,7 +50,55 @@ const togglePlugin = ({ plugin, pluginManager, store }) => {
     };
 };
 
-const startTray = ({ pluginManager, plugins, store }) => {
+const initQuadrakill = async () => {
+    if (!process.env.DISCORD_BOT_TOKEN) {
+        return null;
+    }
+
+    const { default: Quadrakill } = await import('quadrakill');
+    const quadrakill = new Quadrakill({
+        eligiblePlayers: ELIGIBLE_PLAYERS,
+        gameEventsToAudio: GAME_EVENTS_TO_AUDIO,
+    });
+
+    const toggleQuadrakill = () => {
+        if (quadrakill.isDisconnected()) {
+            quadrakill.joinSketchyCloset();
+        } else {
+            quadrakill.leaveSketchyCloset();
+        }
+    };
+
+    return {
+        quadrakill,
+        quadrakillItem: {
+            checked: false,
+            click: toggleQuadrakill,
+            enabled: false,
+            id: 'quadrakill',
+            label: 'Quadrakill Hype Man',
+            type: 'checkbox',
+        },
+    };
+};
+
+const handleQuadrakillEvents = ({ contextMenu, quadrakill, tray }) => {
+    quadrakill.eventEmitter.on('disconnect', () => {
+        contextMenu.getMenuItemById('quadrakill').checked = false;
+        tray.setContextMenu(contextMenu);
+    });
+    quadrakill.eventEmitter.on('ready', () => {
+        contextMenu.getMenuItemById('quadrakill').enabled = true;
+        tray.setContextMenu(contextMenu);
+    });
+};
+
+const quitApp = () => {
+    app.isQuitting = true;
+    app.quit();
+};
+
+const startTray = async ({ pluginManager, plugins, store }) => {
     const tray = new Tray(iconPath);
     const pluginItems = Object.values(plugins).map((plugin) => ({
         checked: plugin.checked,
@@ -51,19 +106,58 @@ const startTray = ({ pluginManager, plugins, store }) => {
         label: plugin.label,
         type: 'checkbox',
     }));
+    const { quadrakill, quadrakillItem } = await initQuadrakill();
     const contextMenu = Menu.buildFromTemplate([
         ...pluginItems,
         { type: 'separator' },
-        { label: 'Quit', role: 'quit' },
+        ...(quadrakillItem ? [quadrakillItem, { type: 'separator' }] : []),
+        {
+            click: openThing,
+            label: 'Settings',
+            type: 'normal',
+        },
+        { type: 'separator' },
+        { click: quitApp, label: 'Quit' },
     ]);
+
+    handleQuadrakillEvents({ contextMenu, quadrakill, tray });
 
     tray.setTitle('Support');
     tray.setToolTip('Support for League of Legends');
     tray.setContextMenu(contextMenu);
 };
 
+let browserWindow;
+const openThing = () => {
+    browserWindow.show();
+};
+
 app.whenReady().then(async () => {
     const store = await loadStore();
     const { pluginManager, plugins } = await startPluginManager(store);
     startTray({ pluginManager, plugins, store });
+
+    browserWindow = new BrowserWindow({
+        backgroundColor: 'purple',
+        center: true,
+        minHeight: 800,
+        minWidth: 800,
+        resizeable: false,
+        show: false,
+        title: 'Support',
+    });
+    browserWindow.loadFile('./settings.html');
+    browserWindow.setMenuBarVisibility(false);
+    browserWindow.on('minimize', (event) => {
+        event.preventDefault();
+        browserWindow.hide();
+    });
+    browserWindow.on('close', (event) => {
+        if (!app.isQuitting) {
+            event.preventDefault();
+            browserWindow.hide();
+        }
+
+        return false;
+    });
 });
---
+++ src/quadrakillSettings.js
@@ -0,0 +1,62 @@
+const prefix = process.cwd();
+
+const ELIGIBLE_PLAYERS = new Set([
+    'Mike Cheek',
+    'baseballover723',
+    'Doomerdinger',
+]);
+
+const GAME_EVENTS_TO_AUDIO = {
+    GameStart: {
+        soundFile: `${prefix}/assets/double-kill.mp3`,
+        volume: 0.4,
+    },
+    MinionsSpawning: {
+        soundFile: `${prefix}/assets/quadraaa-kill.mp3`,
+        volume: 0.4,
+    },
+    FirstBrick: {},
+    TurretKilled: {},
+    InhibKilled: {},
+    DragonKill: {},
+    DragonKillStolen: {},
+    DragonKillElder: {},
+    DragonKillElderStolen: {
+        soundFile: `${prefix}/assets/40IQ.mp3`,
+        volume: 0.4,
+    },
+    HeraldKill: {},
+    HeraldKillStolen: {
+        soundFile:  `${prefix}/assets/40IQ.mp3`,
+        volume: 0.4,
+    },
+    BaronKill: {},
+    BaronKillStolen: {
+        soundFile:  `${prefix}/assets/40IQ.mp3`,
+        volume: 0.4,
+    },
+    ChampionKill: {},
+    Multikill2: {
+        soundFile:  `${prefix}/assets/double-kill.mp3`,
+        volume: 0.5,
+    },
+    Multikill3: {
+        soundFile: `${prefix}/assets/oh=baby-a-triple.mp3`,
+        volume: 0.5,
+    },
+    Multikill4: {
+        soundFile: `${prefix}/assets/quadraaa-kill.mp3`,
+        volume: 0.5,
+    },
+    Multikill5: {
+        soundFile: `${prefix}/assets/penta-kill.mp3`,
+        volume: 0.5,
+    },
+    Ace: {},
+    FirstBlood: {
+        soundFile: `${prefix}/assets/faker-what-was-that.mp3`,
+        volume: 0.5,
+    },
+};
+
+module.exports = { ELIGIBLE_PLAYERS, GAME_EVENTS_TO_AUDIO };
